5cdb543fb958e2a3f802d4a4418b4d9e44516f46
shikha-builds <sshikhatiwarii@gmail.com>
2026-05-17 15:15:04 +0530

    Fixed Railway Gemini API

--- backend/llm.py
+++ backend/llm.py
@@ -1,46 +1,37 @@
-from google import genai
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
+from google import genai
+
 
 client = genai.Client(
     api_key=os.getenv("GEMINI_API_KEY")
 )
 
 
-def get_ai_suggestions(resume_text, jd_text):
+def get_ai_suggestions(
+    resume_text,
+    job_description
+):
 
     prompt = f"""
-    Analyze this resume against the job description.
+    You are an ATS resume expert.
 
-    Give ONLY 10 short bullet point suggestions.
+    Analyze the following resume
+    against the job description.
 
-    Include:
-    1. Missing skills
-    2. ATS improvements
-    3. Resume enhancement tips
-    4. Better action verbs
-    5. Formatting improvements
-
-    Keep every point short, clear, and professional.
+    Give ONLY 10 short bullet-point
+    suggestions to improve the resume.
 
     Resume:
     {resume_text}
 
     Job Description:
-    {jd_text}
+    {job_description}
     """
 
-    try:
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
 
-        response = client.models.generate_content(
-            model="gemini-2.5-flash",
-            contents=prompt
-        )
-
-        return response.text
-
-    except Exception as e:
-
-        return f"Error generating AI suggestions: {str(e)}"
+    return response.text
